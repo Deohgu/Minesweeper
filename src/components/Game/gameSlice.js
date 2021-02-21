866463b2b295377b70d085fb6fca92fcd8dc507a
Deohgu <dioggopsanttos@gmail.com>
2021-02-21 19:41:38 +0000

    bug: Fixes problem with reducers

--- src/components/Game/gameSlice.js
+++ src/components/Game/gameSlice.js
@@ -3,24 +3,26 @@ import { createSlice } from '@reduxjs/toolkit'
 export const gameSlice = createSlice({
   name: 'game',
   initialState: {
-    gridWidth: 10,
-    size: 100,
-    bombs: 20,
+    gridWidth: 5,
+    size: 25,
+    bombs: 2,
     cellArray: [],
     get flaggedAmount () { return this.bombs },
     gameStatus: 'waiting'
   },
   reducers: {
-    updateGridWidth: (state, action) => state.gridWidth = action.payload,
-    updateSize: (state, action) => state.size = action.payload,
-    updateBombs: (state, action) => state.bombs = action.payload,
-    updateCellArray: (state, action) => state.cellArray = action.payload,
-    incrementFlaggedAmount: state => state++,
-    decrementFlaggedAmount: state => state--,
-    updateStatus: (state, action) => state.gameStatus = action.payload
+    updateGridWidth: (state, action) => { state.gridWidth = action.payload },
+    updateSize: (state, action) => { state.size = action.payload },
+    updateBombs: (state, action) => { state.bombs = action.payload },
+    updateCellArray: (state, action) => { state.cellArray = action.payload },
+    incrementFlaggedAmount: state => { state.flaggedAmount++ },
+    decrementFlaggedAmount: state => { state.flaggedAmount-- },
+    resetFlaggedAmount: state => { state.flaggedAmount = state.bombs },
+    // updateStatus: (state, action) => { state.gameStatus = action.payload }
+    updateStatus: (state, action) => { state.gameStatus = action.payload }
   }
 })
 
-export const { updateGridWidth, updateSize, updateBombs, updateCellArray, incrementFlaggedAmount, decrementFlaggedAmount, updateStatus } = gameSlice.actions
+export const { updateGridWidth, updateSize, updateBombs, updateCellArray, incrementFlaggedAmount, decrementFlaggedAmount, resetFlaggedAmount, updateStatus } = gameSlice.actions
 
 export default gameSlice.reducer
